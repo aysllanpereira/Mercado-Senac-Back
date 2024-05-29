e8c17934db59387a01c10680cecc6c6ffdb2f2b8
Aluno <aluno@edu.senac.br>
2024-05-28 21:52:56 -0300

    controladores, rotas e modelo do pedido

--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const controladorProduto = require('../controllers/controladorProduto');
+const controladorPedido = require('../controllers/controladorPedido');
 
 router.post('/produtos', controladorProduto.criarProduto);
 router.get('/produto', controladorProduto.obterProduto);
 router.delete('/produtos', controladorProduto.apagarProduto);
 router.put('/produtos', controladorProduto.editarProdutos);
+
+router.post('/pedido', controladorPedido.criarPedido);
+router.get('/pedido', controladorPedido.obterPedido);
 
 module.exports = router;
