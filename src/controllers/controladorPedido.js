93432141edf5c1fc5c2edbfd4305ce08885f5f1f
Aysllan <aysllansantos097@gmail.com>
2024-05-29 21:35:53 -0300

    Criando rota para pedidos do carrinho

--- src/controllers/controladorPedido.js
+++ src/controllers/controladorPedido.js
@@ -1,12 +1,14 @@
 const Pedido = require("../models/modeloPedido");
 
 exports.criarPedido = async (req, res) => {
+
     try {
+      const { name, addres, payment } = req.body;
+      const carrinho = req.body.carrinho;
+      console.log("aqui = ", req.body)
+      let valor = carrinho.reduce((acc, item) => acc + item.preco, 0)
       
-      const { nome, telefone, carrinho } = req.body;
-      let valor_total = carrinho.reduce((acc, item) => acc + item.precoUnitario, 0)
-      
-      const novoPedido = await Pedido.create({ nome, telefone, valor_total, endereco, itens: JSON.stringify(carrinho) });
+      const novoPedido = await Pedido.create({ nome: name, endereco: addres, forma_pagamento: payment, valor_total: valor, itens: JSON.stringify(carrinho) });
 
       res.status(201).json( novoPedido );
     } catch (error) {
@@ -17,14 +19,16 @@ exports.criarPedido = async (req, res) => {
   exports.obterPedido = async (req, res) => {
     try {
       const idPedido = req.body.idPedido;
-  
+      console.log("pedido = ", req.body)
       if (idPedido) {
         const pedido = await Pedido.findByPk(idPedido)
         res.json(pedido);
+        // console.log("aqui ", pedido)
       }else{
         const pedidos = await Pedido.findAll();
         res.json(pedidos);
       }
+      res.status(201).json(idPedido);
     } catch (error) {
       res.status(400).json({ error: error.message });
     }
